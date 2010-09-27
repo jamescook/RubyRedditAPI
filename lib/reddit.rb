a4c24b8776a4af8b028c41f74c6c65bf38e9a2c6
james cook <jamecook@gmail.com>
2010-09-26 23:44:44 -0400

    Basic API behavior. Added ability to parse submissions and vote on submissions.

---
+++ lib/reddit.rb
@@ -0,0 +1,112 @@
+$LOAD_PATH.unshift File.dirname(__FILE__)
+
+require "httparty"
+require "json"
+require "reddit/submission"
+
+module Reddit
+
+  class Api
+    include HTTParty
+
+    attr_reader :cookie, :user, :password, :last_action, :debug
+    attr_accessor :modhash
+    base_uri "www.reddit.com"
+
+    def initialize(user,password, options={})
+      @user     = user
+      @password = password
+      @debug    = StringIO.new
+    end
+
+    def inspect
+      "<Reddit::Api user='#{user}'>"
+    end
+
+    def user_agent
+      "Ruby Reddit Client v0.0.1"
+    end
+
+    def logged_in?
+      !!cookie
+    end
+
+    def browse(subreddit, options={})
+      subreddit = sanitize_subreddit(subreddit)
+      read("/r/#{subreddit}.json", options )
+    end
+
+    # TODO Move voting to separate class
+    def vote(direction, submission_id)
+      return false unless logged_in?
+      up_or_down = direction == :up ? 1 : -1
+      url        = action_mapping["vote"]["path"]
+      self.class.post( url, {:body => {:id => submission_id, :dir => up_or_down, :uh => modhash}, :headers => base_headers, :debug_output => @debug} )
+    end
+
+    def upvote(submission)
+      vote(:up, submission.id)
+    end
+
+    def downvote(submission)
+      vote(:down, submission.id)
+    end
+
+    def read(url, options={})
+      unless throttled?
+        @debug.rewind
+        resp = self.class.send( (options[:verb] || "get"), url, {:query => (options[:query] || {}), :headers => base_headers, :debug_output => @debug})
+        if valid_response?(resp)
+          @last_action = Time.now
+          resp = Reddit::Submission.parse( self, JSON.parse(resp.body) )
+          return resp
+        end
+      end
+    end
+
+    def login
+      url = action_mapping["login"]["path"]
+      capture_session(self.class.post( url, {:body => {:user => user, :passwd => password}, :debug_output => @debug} ) )
+      logged_in?
+    end
+
+    protected
+    def valid_response?(response)
+      response.code == 200
+    end
+
+    def capture_session(response)
+      cookies = response.headers["set-cookie"]
+      @cookie = cookies
+    end
+
+    def throttled?
+      @last_action && ( ( Time.now - @last_action ) < 1.0 )
+    end
+
+    def sanitize_subreddit(subreddit)
+      subreddit.gsub!(/^\/?r\/?/,'')
+      subreddit.gsub!(/\.json\Z/,'')
+      subreddit
+    end
+
+    def action_mapping
+      {
+        "login"       =>  {"path" => "/api/login",     "verb" => "POST"},
+        "vote"        =>  {"path" => "/api/vote",      "verb" => "POST"},
+        "save"        =>  {"path" => "/api/save",      "verb" => "POST"},
+        "unsave"      =>  {"path" => "/api/unsave",    "verb" => "POST"},
+        "comment"     =>  {"path" => "/api/comment",   "verb" => "POST"},
+        "subscribe"   =>  {"path" => "/api/subscribe", "verb" => "POST" },
+        "comments"    =>  {"path" => "/comments",      "verb" => "GET" },
+        "my_reddits"  =>  {"path" => "/reddits/mine",  "verb" => "GET" },
+        "saved"       =>  {"path" => "saved",          "verb" => "GET"},
+        ""            =>  {"path" => "",               "verb" => "GET"}
+      }
+    end
+
+    def base_headers
+      {'Cookie' => cookie.to_s, 'user-agent' => user_agent}
+    end
+  end
+end
---
+++ lib/reddit/submission.rb
@@ -0,0 +1,56 @@
+module Reddit
+  class Submission
+    attr_reader :session, :domain, :media_embed, :subreddit, :selftext_html, :selftext, :likes, :saved, :clicked, :author, :media, :score, :over_18, :hidden, :thumbnail, :subreddit_id, :downs, :is_self, :permalink, :name, :created, :url, :title, :created_utc, :num_comments, :ups
+
+    def initialize(session,data)
+      @session = session
+      json = data
+      parse(json)
+    end
+
+    def inspect
+      "<Reddit::Submission id='#{id}' author='#{author}' title='#{title}'>"
+    end
+
+    def id
+      #TODO Don't hardcode t3, it's in the JSON
+      "t3_#{@id}"
+    end
+
+    def reload
+      #TODO
+    end
+
+    def upvote
+      session.upvote(self)
+    end
+
+    def downvote
+      session.downvote(self)
+    end
+
+    def comments
+      #TODO
+    end
+
+    def parse(json)
+      json.keys.each do |key|
+        instance_variable_set("@#{key}", json[key])
+      end
+    end
+
+    class << self
+      def parse(session,json)
+        submissions = []
+        data            = json["data"]
+        session.modhash = data["modhash"] # Needed for api calls
+        children        = data["children"]
+        children.each do |child|
+          data = child["data"]
+          submissions << Reddit::Submission.new(session,data)
+        end
+        submissions
+      end
+    end
+  end
+end
